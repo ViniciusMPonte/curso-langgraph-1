5dab9d3b32ab82f524eddf562b05d1e00210cadc
vinicius <vinicius.mponte@gmail.com>
2026-05-24 12:37:40 -0300

    feat: adição de exemplo.py

---
+++ exemplo.py
@@ -0,0 +1,35 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage, AIMessage
+from langgraph import graph
+from langgraph.graph import StateGraph
+from langgraph.graph.state import CompiledStateGraph
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
+llm_model: ChatGoogleGenerativeAI = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash-lite",
+    google_api_key=API_KEY
+)
+
+class StateSchema(BaseModel):
+    input: str
+    output: str
+
+def responder(state: StateSchema):
+    response: AIMessage = llm_model.invoke([HumanMessage(content=state.input)])
+    return StateSchema(input=state.input, output=str(response.content))
+
+graph: StateGraph = StateGraph(StateSchema)
+graph.add_node('responder', responder)
+graph.set_entry_point('responder')
+graph.set_finish_point('responder')
+
+export_graph: CompiledStateGraph = graph.compile()
+
+if __name__ == "__main__":
+    result = export_graph.invoke(StateSchema(input='Quem descobriu o Brasil?', output=''))
+    print(result)
